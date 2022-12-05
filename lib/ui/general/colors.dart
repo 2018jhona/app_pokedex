d6f7c487a485c691ade22d557cad659da44fb19f
jhonatan <201800610I@utea.edu.pe>
2022-12-05 10:07:12 -0500

    versn 24

---
+++ lib/ui/general/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Map<String, Color> colorsPokemon = {
+  "Grass": Color.fromARGB(255, 60, 184, 155),
+  "Bug": Color(0xffC0C0C0),
+  "Fire": Color.fromARGB(255, 233, 98, 98),
+  "Water": Color(0xff76BCFD),
+  "Electric": Color(0xffFFD86F),
+  "Normal": Color(0xff030303),
+  "Ground": Color(0xff808000),
+  "Poison": Color(0xff800000),
+  "Rock": Color(0xff008080),
+  "Psychic": Color(0xffC77AEE),
+  "Fighting": Color(0xff753E13),
+  "Ice": Color(0xff29F4F1),
+  "Dragon": Color(0xffF8AD58),
+};
